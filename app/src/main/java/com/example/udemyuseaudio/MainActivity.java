dd84d3d760612f2803fb9a3fe082f1906ca53010
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-12 17:58:47 +0300

    [Add] play pause buttons

--- app/src/main/java/com/example/udemyuseaudio/MainActivity.java
+++ app/src/main/java/com/example/udemyuseaudio/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.udemyuseaudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        player = MediaPlayer.create(getApplicationContext(), R.raw.original);
+    }
+
+    public void togglePlay(View view) {
+        player.start();
+    }
+
+    public void pause(View view) {
+        player.pause();
     }
 }
