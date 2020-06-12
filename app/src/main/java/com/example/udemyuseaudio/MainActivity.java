e3211f56c2c0d89f2c2ecadc9ae7f85bab31b546
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-12 18:12:08 +0300

    [Add] volume regulator

--- app/src/main/java/com/example/udemyuseaudio/MainActivity.java
+++ app/src/main/java/com/example/udemyuseaudio/MainActivity.java
@@ -2,23 +2,49 @@ package com.example.udemyuseaudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer player;
     Button playButton;
+    SeekBar volumeBar;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
         player = MediaPlayer.create(getApplicationContext(), R.raw.original);
         playButton = findViewById(R.id.playButton);
+        volumeBar = findViewById(R.id.volumeBar);
+        volumeBar.setMax(maxVolume);
+
+        volumeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void togglePlay(View view) {
