131559f5e46001fb6c94f8c0e9bcb5d6f24d2415
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-12 17:59:15 +0300

    [Update] play pause by one button

--- app/src/main/java/com/example/udemyuseaudio/MainActivity.java
+++ app/src/main/java/com/example/udemyuseaudio/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer player;
+    Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         player = MediaPlayer.create(getApplicationContext(), R.raw.original);
+        playButton = findViewById(R.id.playButton);
     }
 
     public void togglePlay(View view) {
-        player.start();
-    }
-
-    public void pause(View view) {
-        player.pause();
+        if (player.isPlaying()) {
+            player.pause();
+            playButton.setText("Play");
+        } else  {
+            player.start();
+            playButton.setText("Pause");
+        }
     }
 }
